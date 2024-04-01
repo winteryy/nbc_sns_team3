a67f69ec4bbea2f3ebea716677f3634df0899d33
lany159 <tnfl2644@gmail.com>
2024-04-01 16:43:57 +0900

    feat/toast

--- app/src/main/java/com/nbcteam3/nbcsns3/SignInActivity.kt
+++ app/src/main/java/com/nbcteam3/nbcsns3/SignInActivity.kt
@@ -1,9 +1,13 @@
 package com.nbcteam3.nbcsns3
 
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Message
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,10 +16,38 @@ class SignInActivity : AppCompatActivity() {
 
         val signInButton = findViewById<Button>(R.id.signInButton)
         val signUpButton = findViewById<Button>(R.id.signUpButton)
+        val userId = findViewById<EditText>(R.id.userId)
+        val inputUserId = userId.text.toString()
+        val userPw = findViewById<EditText>(R.id.userPw)
+        val inputUserPw = userPw.text.toString()
 
-        signInButton.setOnClickListener {
+        val isId = inputUserId.isEmpty()
+        val isPw = inputUserPw.isEmpty()
+
+        signUpButton.setOnClickListener {
             val intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
         }
+        signInButton.setOnClickListener {
+
+            if(isId){
+                showToast("@string/inputId")
+                return@setOnClickListener
+            }
+            if (isPw){
+                showToast("비밀번호를 입력해주세요")
+                return@setOnClickListener
+            }
+
+            val sharedPreference = getSharedPreferences("signInUser", MODE_PRIVATE)
+            val editor : SharedPreferences.Editor = sharedPreference.edit()
+            editor.putString("userId",userId.toString())
+            editor.putString("userPw",userPw.toString())
+//                    val intent = Intent(this, 메인엑티비티::class.java)
+//                    startActivity(intent)
+                }
+    }
+    private fun showToast(message: String){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
--- app/src/main/java/com/nbcteam3/nbcsns3/SignUpActivity.kt
+++ app/src/main/java/com/nbcteam3/nbcsns3/SignUpActivity.kt
@@ -1,11 +1,41 @@
 package com.nbcteam3.nbcsns3
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        val signUpButton = findViewById<Button>(R.id.signUpButton)
+        val signUpName = findViewById<EditText>(R.id.signUpName)
+        val inputUserName = signUpName.text.toString()
+        val signUpId = findViewById<EditText>(R.id.signUpID)
+        val inputUserId = signUpId.text.toString()
+        val signUpPw = findViewById<EditText>(R.id.signUpPw)
+        val inputUserPw = signUpPw.text.toString()
+
+        val isName = inputUserName.isEmpty()
+        val isId = inputUserId.isEmpty()
+        val isPw = inputUserPw.isEmpty()
+
+        signUpButton.setOnClickListener {
+
+            if (isName || isId || isPw){
+                showToast("입력되지 않은 정보가 있습니다")
+            }
+
+            val intent = Intent(this, SignInActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+    private fun showToast(message: String){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
