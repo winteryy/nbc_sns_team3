61d148953618ca87592d07b6414987e6f0e170f0
yoohyejeong <hara3129@gmail.com>
2024-04-03 18:06:00 +0900

    chore: Save current state

--- app/src/main/java/com/nbcteam3/nbcsns3/DetailActivity.kt
+++ app/src/main/java/com/nbcteam3/nbcsns3/DetailActivity.kt
@@ -1,11 +1,42 @@
 package com.nbcteam3.nbcsns3
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class DetailActivity : AppCompatActivity() {
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
+
+
+        val button = findViewById<Button>(R.id.setMainButton)
+        val userimage = findViewById<ImageView>(R.id.setUserimage)
+        val userid = findViewById<TextView>(R.id.setUserId)
+        val username = findViewById<TextView>(R.id.setUserName)
+        val storyImage = findViewById<ImageView>(R.id.setStory)
+        val userstory = findViewById<TextView>(R.id.userStory)
+
+
+        //user.setImageDrawable(AppCompatResources.getDrawable(this, id))
+
+
+        button.setOnClickListener {
+
+           // val intent = Intent(this, MyPageActivity::class.Java)
+
+          //  startActivity(intent)
+
+
+        }
+
+
+
     }
 }
