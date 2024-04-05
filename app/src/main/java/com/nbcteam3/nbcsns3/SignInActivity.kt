cfcb31c364ad09c313004ef1aadb8ceafaf06de9
fbalt <d456456@naver.com>
2024-04-05 10:52:25 +0900

    ìë로그인 값 찾기

--- app/src/main/java/com/nbcteam3/nbcsns3/SignInActivity.kt
+++ app/src/main/java/com/nbcteam3/nbcsns3/SignInActivity.kt
@@ -1,14 +1,18 @@
 package com.nbcteam3.nbcsns3
 
+
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class SignInActivity : AppCompatActivity() {
+
+    private val loadUsers = DummyServer.loadUsers()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
@@ -18,30 +22,46 @@ class SignInActivity : AppCompatActivity() {
         val userId = findViewById<EditText>(R.id.userId)
         val userPw = findViewById<EditText>(R.id.userPw)
 
+
         signUpButton.setOnClickListener {
             val intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
         }
         signInButton.setOnClickListener {
+            val userId2 = userId.text.toString()
+            val userPw2 = userPw.text.toString()
 
-            if (userId.text.isEmpty()) {
-                showToast(getString(R.string.input_id))
-                return@setOnClickListener
+            Log.d("id",userId2)
+            Log.d("pw",userPw2)
+            val findUser = loadUsers.find { it.userId == userId2 }
+            val findPw = loadUsers.find { it.password == userPw2 }
+
+                   if (userId.text.isEmpty()) {
+                       showToast(getString(R.string.input_id))
+                       return@setOnClickListener
+                   }
+
+                   if (userPw.text.isEmpty()) {
+                       showToast(getString(R.string.input_pw))
+                       return@setOnClickListener
+                   }
+
+                   val sharedPreference = getSharedPreferences("signInUser", MODE_PRIVATE)
+                   val editor: SharedPreferences.Editor = sharedPreference.edit()
+                   editor.putString("userId", userId.text.toString())
+                   editor.putString("userPw", userId.text.toString())
+                   editor.apply()
+
+            if (findUser !== null) {
+
+                if (findPw !== null){
+
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                }
             }
-            if (userPw.text.isEmpty()) {
-                showToast(getString(R.string.input_pw))
-                return@setOnClickListener
-            }
-
-            val sharedPreference = getSharedPreferences("signInUser", MODE_PRIVATE)
-            val editor: SharedPreferences.Editor = sharedPreference.edit()
-            editor.putString("userId", userId.text.toString())
-            editor.putString("userPw", userPw.text.toString())
-            editor.apply()
-
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
         }
+
     }
 
     private fun showToast(message: String) {
